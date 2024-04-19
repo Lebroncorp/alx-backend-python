90e3dee2d36168d2365acba84de69750073b3a05
Lebroncorp <lebroncorp@yahoo.com>
2024-04-19 13:45:54 +0000

    strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module for task 1
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-annotated function that takes a string str1 and concatenates
+    it to a second string str2 and returns a concatenated string str.
+
+    Args:
+        str1 (str): first string to concatenate.
+        str2 (str): second string to concatenate.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for task 2
+"""
+
+
+def floor(a: float) -> int:
+    """
+    Type-annotated function that takes a float n as argument
+    and returns the floor integer value of the float.
+
+    Args:
+        a (float): float to floor.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return int(a)
