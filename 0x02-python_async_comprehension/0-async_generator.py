9db3bce40d1e02372941141268e0a2884b1e264f
Lebroncorp <lebroncorp@yahoo.com>
2024-04-28 23:51:31 +0000

    runtime

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A coroutine called async_generator that takes no arguments"""
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async generator loops 10 times and
+    waits 1 second each time and then
+    yield a random number
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
